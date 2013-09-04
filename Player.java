29e4aa5bbd920c87671773c0c201c981e0a58aef
tusharnarayan <tusharnarayan@gmail.com>
2013-09-04 01:16:42 -0700

    In the process of adding all the evaluation function's helpers

--- Player.java
+++ Player.java
@@ -5,6 +5,7 @@ import java.util.List;
  * Class to implement the player of the game
  * 
  * @author bli
+ * @author tnarayan
  * 
  */
 public class Player {
@@ -12,6 +13,9 @@ public class Player {
 	int timeLimit;
 	boolean isMax;
 
+	/**
+	 * Constructor
+	 */
 	public Player(int playerNumber, int timeLimit, boolean isMax) {
 		this.playerNumber = playerNumber;
 		this.timeLimit = timeLimit;
@@ -29,12 +33,88 @@ public class Player {
 		String move = "2 1";
 		return move;
 	}
+	
+	/**
+	 * Function to check if a particular drop move is legally allowed on the board
+	 * Since empty spots are represented by '9', if there is an empty spot in the column
+	 * that the drop is being attempted in, it will be allowed.
+	 * 
+	 * @param gameBoard
+	 * @param dropInColumn the column number that the drop move is being attempted in
+	 * 
+	 * @return boolean indicating if the drop move is legal
+	 * 
+	 */
+	public boolean isDropMoveLegal(Board gameBoard, int dropInColumn){
+		for (int i : gameBoard.height){
+			if (gameBoard[i][dropInColumn] == 9){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int evaluateTokensInARowVertically(int numTokens, Board gameBoard, int numRow, int numColumn){
+		
+	}
+	
+	/**
+	 * @param numTokens the N-in-a-row number of tokens being searched for
+	 * @param playerNum the player whose tokens are being searched
+	 * @param gameBoard
+	 * 
+	 * @return the number of tokens encountered for the specified N-in-a-row
+	 */
+	public int evaluateTokensInARow(int numTokens, int playerNum, Board gameBoard){
+		int countOfTokensEncountered = 0;
+		
+		for(int i = 0; i < gameBoard.height; i++){
+			for(int j = 0; j < gameBoard.width; i++){
+				if(gameBoard[i][j] == playerNum){//check only our tokens; ignore the opponent's tokens
+					countOfTokensEncountered += evaluateTokensInARowVertically(numTokens, gameBoard, i, j);
+					countOfTokensEncountered += evaluateTokensInARowHorizontally(numTokens, gameBoard, i, j);
+					countOfTokensEncountered += evaluateTokensInARowDiagonallyLeft(numTokens, gameBoard, i, j);
+					countOfTokensEncountered += evaluateTokensInARowDiagnoallyRight(numTokens, gameBoard, i, j);
+				}
+			}
+		}
+		
+		return countOfTokensEncountered;
+	}
+	
+	/**
+	 * Evaluates the relative preferability of the board
+	 * fours-in-a-row are weighted more than threes-in-a-row, which in turn
+	 * are weighted more than twos-in-a-row
+	 * 
+	 * @param gameBoard
+	 * @param playerNum
+	 * 
+	 */
+	public int evaluateBoard(Board gameBoard, int playerNum){
+		int ourScoreFoursInARow = evaluateTokensInARow(4, playerNum, gameBoard);
+		int ourScoreThreesInARow = evaluateTokensInARow(3, playerNum, gameBoard);
+		int ourScoreTwosInARow = evaluateTokensInARow(2, playerNum, gameBoard);
+		int ourTotalScore = ourScoreFoursInARow + ourScoreThreesInARow + ourScoreTwosInARow;
+		
+		//TODO: is this the acceptable way of determining the opponent's number?
+		int opponentPlayerNum = (playerNum == 1?) 2 : 1;
+		
+		int opponentScoreFoursInARow = evaluateTokensInARow(4, opponentPlayerNum, gameBoard);
+		int opponentScoreThreesInARow = evaluateTokensInARow(3, opponentPlayerNum, gameBoard);
+		int opponentScoreTwosInARow = evaluateTokensInARow(2, opponentPlayerNum, gameBoard);
+		int opponentTotalScore = opponentScoreFoursInARow + opponentScoreThreesInARow + opponentScoreTwosInARow;
+		
+		return ourTotalScore - opponentTotalScore;
+	}
 
 	/**
 	 * Function for the player to make a move based on a string
 	 * 
 	 * @param moveToTake
 	 *            The move the player is going to make
+	 * @param playerNum
+	 * 			   represents the current player
 	 * @param gameBoard
 	 *            The board on which the player makes the move on
 	 * @throws Connect4Exception
@@ -52,7 +132,7 @@ public class Player {
 			} else {
 				throw new Connect4Exception("Drop: Move illegal");
 			}
-		} else {
+		} else { //the move made was to remove a disc
 			if (gameBoard.canRemoveADiscFromBottom(col, playerNum)) {
 				gameBoard.removeADiscFromBottom(col);
 			} else {
